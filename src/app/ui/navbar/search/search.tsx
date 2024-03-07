d9a2212e152366c44c495da2f99dd28802877f17
User <user@Users-MacBook-Pro.local>
2024-03-07 16:19:04 +0100

    added Link to search to be able to navigate to individual coin page

--- src/app/ui/navbar/search/search.tsx
+++ src/app/ui/navbar/search/search.tsx
@@ -3,12 +3,13 @@ import React, {useState} from "react";
 import { coinsTableData } from "@/app/lib/placerholder-data";
 import { CoinsTableData } from "@/app/lib/definitions";
 import SearcIcon from "../icons/searchIcon";
+import Link from "next/link";
 
 const styles = {
     main: 'relative w-[356px] h-[48px]',
     input: 'w-full h-full bg-[#1F1934] text-[#D1D1D1] text-sm border border-slate-700 rounded pl-10 pr-4 py-2',
     searchIcon: 'absolute left-3 top-3.5',
-    searchDropdown: 'absolute top-14 z-10 w-[356px] bg-[#1F1934] text-sm text-white pl-4 py-2 border-l border-r border-b rounded-b border-slate-700'
+    searchDropdown: 'absolute top-14 z-10 flex flex-col w-[356px] bg-[#1F1934] text-sm text-white pl-4 py-2 border-l border-r border-b rounded-b border-slate-700'
 }
 
 const Search = () => {
@@ -28,7 +29,7 @@ const Search = () => {
             <div className={styles.searchIcon}><SearcIcon/></div>
             <input className={styles.input} placeholder="Search coin..." value={value} onChange={(e) => handleSearch(e.target.value)}/>
             {value !== '' && <div className={styles.searchDropdown}>
-                {searchList.map(el => <p className="mb-1">{el.name}</p>)}
+                {searchList.map(el => <Link href={`/coin/${el.id}`} className="mb-1 hover:text-[#D878FA] ease-in-out" onClick={() => setValue('')}>{el.name}</Link>)}
                 </div>}
             </div>
             </>
