2d558a027e0c7e89f3fb250fc4df8a7c2c82a4ed
User <user@Users-MacBook-Pro.local>
2024-04-22 09:45:14 +0200

    added header

--- src/app/convertor/page.tsx
+++ src/app/convertor/page.tsx
@@ -1,19 +1,18 @@
 "use client";
 import { useAppSelector } from "../lib/hooks";
-import { styles } from "./styles";
+import { setStyles } from "./styles";
 import Link from "next/link";
 
 export default function Page() {
   const { isDark } = useAppSelector((state) => state.theme);
-  const mainBg = isDark ? "bg-dark-primaryBg" : "bg-slate-200";
-  const buttonColors = isDark
-    ? "bg-[#232337] text-dark-textPrimary"
-    : "bg-[#FFFF] text-light-textSecondary";
+  const styles = setStyles(isDark);
+  let date: Date | string = new Date();
+  date = date.toLocaleString();
 
   return (
-    <main className={`${styles.main} ${mainBg}`}>
-      <div className="flex">
-        <Link href={"/"} className={`${styles.button} ${buttonColors}`}>
+    <main className={`${styles.main} ${styles.mainBg}`}>
+      <div className="flex mb-10">
+        <Link href={"/"} className={`${styles.button} ${styles.buttonColors}`}>
           Coins
         </Link>
         <Link
@@ -22,6 +21,12 @@ export default function Page() {
         >
           Convertor
         </Link>
+      </div>
+      <div className="flex flex-col gap-2">
+        <h3 className={`${styles.primaryText} text-xl`}>
+          Online currency convertor
+        </h3>
+        <h3 className={`${styles.secondaryText} text-base`}>{date}</h3>
       </div>
     </main>
   );
--- src/app/convertor/styles.ts
+++ src/app/convertor/styles.ts
@@ -1,6 +1,14 @@
-export const styles = {
-  main: "flex min-h-screen flex-col px-20 py-6 border",
-  button:
-    "w-[244px] h-[45px] flex justify-center items-center bg-[#232337] bg-opacity-50 transition-bg ease-in-out rounded",
-  active: "bg-[#6161de] border border-opacity-60 border-[#7878FF] text-white",
+export const setStyles = (condition: boolean) => {
+  return {
+    main: "flex min-h-screen flex-col px-20 py-6 border",
+    mainBg: condition ? "bg-dark-primaryBg" : "bg-slate-200",
+    button:
+      "w-[244px] h-[45px] flex justify-center items-center bg-[#232337] bg-opacity-50 transition-bg ease-in-out rounded",
+    buttonColors: condition
+      ? "bg-[#232337] text-dark-textPrimary"
+      : "bg-[#FFFF] text-light-textSecondary",
+    active: "bg-[#6161de] border border-opacity-60 border-[#7878FF] text-white",
+    primaryText: condition ? "text-white" : "text-[#424286]",
+    secondaryText: condition ? "text-[#9E9E9E]" : "text-[#424286CC]",
+  };
 };
